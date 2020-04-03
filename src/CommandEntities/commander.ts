b8f2fdebebfb95eac493184cd22ca56bc81d548c
Gianmarco Pettinato <gianmarco.pettinato@gmail.com>
2020-04-03 15:55:32 +0200

    aggiunto disconnect su Command

--- src/CommandEntities/commander.ts
+++ src/CommandEntities/commander.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import NetworkUtils from '../NetworkEntities/networkUtils';
 import { Command } from './command';
 
 const commander = require('commander');
