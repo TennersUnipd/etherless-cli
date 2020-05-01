ce2158e5cc41ca92121314afa6ef17e1da699abb
nicola <nicola.salvadore@gmail.com>
2020-05-01 15:22:13 +0200

    indent fix

--- src/CommandEntities/logCommand.ts
+++ src/CommandEntities/logCommand.ts
@@ -3,27 +3,26 @@ import { rejects } from 'assert';
 var fs = require('fs');
 
 class LogCommand extends Command {
-    COMMAND_NAME = 'log';
+  COMMAND_NAME = 'log';
 
-    COMMAND_ALIAS = 'll';
+  COMMAND_ALIAS = 'll';
 
-    COMMAND_DESCRIPTION = 'list of the latest 20 functions run logged';
+  COMMAND_DESCRIPTION = 'list of the latest 20 functions run logged';
 
-    exec(): Promise<any> {    
-      return new Promise((resolve, rejects) => {
-          try{
-            resolve(JSON.parse(fs.readFileSync('logs.json', 'utf-8')));           
-          }
-          catch(error){
-              if(error.code === 'ENOENT') {
-                rejects("No functions run and no log generated");
-              } else {
-                  rejects("generic error");
-              }
-          }          
+  exec(): Promise<any> {    
+    return new Promise((resolve, rejects) => {
+      try{
+        resolve(JSON.parse(fs.readFileSync('logs.json', 'utf-8')));           
+      }
+      catch(error){
+        if(error.code === 'ENOENT') {
+          rejects("No functions run and no log generated");
+        } else {
+            rejects("generic error");
         }
-      );
-    }
+      }          
+    });
+  }
 }
 
 export default LogCommand;
--- src/log.ts
+++ src/log.ts
@@ -1,39 +1,39 @@
 var fs = require('fs');
 
 export default class Logger{
-    private logs: Log[];
-    constructor(log: Log){
-        this.loadJSON();
-        this.addLog(log);
-        this.saveOnJSON();
-    }
-    private addLog(log: Log){
-        if(this.logs.unshift(log) > 20 ){
-            this.logs = this.logs.slice(0,20);
-        };
-    }
-    private saveOnJSON(){
-        const jsonlogs = JSON.stringify(this.logs);
-        fs.writeFile('logs.json', jsonlogs, (error) => {
-            if(error) console.log(error);
-        });
-    }
-    private loadJSON(){    
-        try{
-            this.logs = JSON.parse(fs.readFileSync('logs.json', 'utf-8'));
-        }
-        catch(err){
-            if(err.code === 'ENOENT') {
-                this.logs = new Array();
-            } else {
-                throw err;
-            }
-        }
-    }
+	private logs: Log[];
+	constructor(log: Log){
+		this.loadJSON();
+		this.addLog(log);
+		this.saveOnJSON();
+	}
+	private addLog(log: Log){
+		if(this.logs.unshift(log) > 20 ){
+			this.logs = this.logs.slice(0,20);
+		};
+	}
+	private saveOnJSON(){
+		const jsonlogs = JSON.stringify(this.logs);
+		fs.writeFile('logs.json', jsonlogs, (error) => {
+			if(error) console.log(error);
+		});
+	}
+	private loadJSON(){    
+		try{
+				this.logs = JSON.parse(fs.readFileSync('logs.json', 'utf-8'));
+		}
+		catch(err){
+			if(err.code === 'ENOENT') {
+					this.logs = new Array();
+			} else {
+					throw err;
+			}
+		}
+	}
 }
 
 interface Log{
-    fname: string;
-    fdate: string;
-    fcost: Number;
+	fname: string;
+	fdate: string;
+	fcost: Number;
 }
