1fccfd582e83de7eba5e6220329f247c9024868e
Simone Franconetti <zumino1997@gmail.com>
2020-05-03 17:50:49 +0200

    Trasferita gestione comando log in commander (funzionante)

--- src/CommandEntities/logCommand.ts
+++ src/CommandEntities/logCommand.ts
@@ -9,11 +9,20 @@ class LogCommand extends Command {
 
   COMMAND_DESCRIPTION = 'list of the latest 20 functions run logged';
 
+  // eslint-disable-next-line class-methods-use-this
   exec(): Promise<string> {
     return new Promise((resolve, rejects) => {
       try {
-        console.log(`${this.COMMAND_NAME} running...`);
-        resolve(JSON.parse(fs.readFileSync('logs.json', 'utf-8')));
+        let logResult = '';
+        const logsContent = JSON.parse(fs.readFileSync('logs.json', 'utf-8'));
+        logsContent.forEach((item) => {
+          const objectKeys = Object.keys(item);
+          objectKeys.forEach((k) => {
+            logResult = `${logResult}\n${k}: ${item[k]}`;
+          });
+          logResult += '\n';
+        })
+        resolve(logResult);
       } catch (error) {
         if (error.code === 'ENOENT') {
           rejects(new Error('No functions run and no log generated'));
